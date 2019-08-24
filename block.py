951ae36eb618c359fd4104ad63b834934a26fd42
Akshay <akshay2104.singh@gmail.com>
2019-08-24 12:20:05 +0530

    added hash function

--- block.py
+++ block.py
@@ -1,6 +1,13 @@
+import hashlib
+
+
 class Block:
     def __init__(self, index, data, timestamp, previous_hash=0):
-        self.index = index
-        self.data = data
-        self.timestamp = timestamp
-        self.previous_hash = previous_hash
+        self.__index = index
+        self.__data = data
+        self.__timestamp = timestamp
+        self.__self_hash = self.__hash_data()
+        self.__previous_hash = previous_hash
+
+    def __hash_data(self):
+        return hashlib.sha256(str(self.__index) + self.__data + self.__timestamp).hexdigest()
